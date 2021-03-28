22913c186e54bf3615b92e1120065071fa898f79
pengshuxue <pengshuxue@megvii.com>
2021-03-28 09:45:44 +0800

    加注释

--- test/feature_initialization_test.cpp
+++ test/feature_initialization_test.cpp
@@ -19,7 +19,6 @@
 #include <msckf_vio/cam_state.h>
 #include <msckf_vio/feature.hpp>
 
-
 using namespace std;
 using namespace Eigen;
 using namespace msckf_vio;
@@ -30,7 +29,8 @@ Isometry3d CAMState::T_cam0_cam1 = Isometry3d::Identity();
 // Static member variables in Feature class
 Feature::OptimizationConfig Feature::optimization_config;
 
-TEST(FeatureInitializeTest, sphereDistribution) {
+TEST(FeatureInitializeTest, sphereDistribution)
+{
   // Set the real feature at the origin of the world frame.
   Vector3d feature(0.5, 0.0, 0.0);
 
@@ -41,33 +41,34 @@ TEST(FeatureInitializeTest, sphereDistribution) {
   // of the camera frames are facing the origin.
   vector<Isometry3d> cam_poses(6);
   // Positive x axis.
-  cam_poses[0].linear() << 0.0,  0.0, -1.0,
-    1.0,  0.0,  0.0, 0.0, -1.0,  0.0;
-  cam_poses[0].translation() << 1.0,  0.0,  0.0;
+  cam_poses[0].linear() << 0.0, 0.0, -1.0,
+      1.0, 0.0, 0.0, 0.0, -1.0, 0.0;
+  cam_poses[0].translation() << 1.0, 0.0, 0.0;
   // Positive y axis.
-  cam_poses[1].linear() << -1.0,  0.0,  0.0,
-     0.0,  0.0, -1.0, 0.0, -1.0,  0.0;
-  cam_poses[1].translation() << 0.0,  1.0,  0.0;
+  cam_poses[1].linear() << -1.0, 0.0, 0.0,
+      0.0, 0.0, -1.0, 0.0, -1.0, 0.0;
+  cam_poses[1].translation() << 0.0, 1.0, 0.0;
   // Negative x axis.
-  cam_poses[2].linear() << 0.0,  0.0,  1.0,
-    -1.0,  0.0,  0.0, 0.0, -1.0,  0.0;
-  cam_poses[2].translation() << -1.0,  0.0,  0.0;
+  cam_poses[2].linear() << 0.0, 0.0, 1.0,
+      -1.0, 0.0, 0.0, 0.0, -1.0, 0.0;
+  cam_poses[2].translation() << -1.0, 0.0, 0.0;
   // Negative y axis.
-  cam_poses[3].linear() << 1.0,  0.0,  0.0,
-     0.0,  0.0,  1.0, 0.0, -1.0,  0.0;
-  cam_poses[3].translation() << 0.0, -1.0,  0.0;
+  cam_poses[3].linear() << 1.0, 0.0, 0.0,
+      0.0, 0.0, 1.0, 0.0, -1.0, 0.0;
+  cam_poses[3].translation() << 0.0, -1.0, 0.0;
   // Positive z axis.
-  cam_poses[4].linear() << 0.0, -1.0,  0.0,
-    -1.0,  0.0,  0.0, 0.0, 0.0, -1.0;
-  cam_poses[4].translation() << 0.0,  0.0,  1.0;
+  cam_poses[4].linear() << 0.0, -1.0, 0.0,
+      -1.0, 0.0, 0.0, 0.0, 0.0, -1.0;
+  cam_poses[4].translation() << 0.0, 0.0, 1.0;
   // Negative z axis.
-  cam_poses[5].linear() << 1.0,  0.0,  0.0,
-     0.0,  1.0,  0.0, 0.0,  0.0,  1.0;
-  cam_poses[5].translation() << 0.0,  0.0, -1.0;
+  cam_poses[5].linear() << 1.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0, 0.0, 1.0;
+  cam_poses[5].translation() << 0.0, 0.0, -1.0;
 
   // Set the camera states
   CamStateServer cam_states;
-  for (int i = 0; i < 6; ++i) {
+  for (int i = 0; i < 6; ++i)
+  {
     CAMState new_cam_state;
     new_cam_state.id = i;
     new_cam_state.time = static_cast<double>(i);
@@ -79,10 +80,11 @@ TEST(FeatureInitializeTest, sphereDistribution) {
 
   // Compute measurements.
   random_numbers::RandomNumberGenerator noise_generator;
-  vector<Vector4d, aligned_allocator<Vector4d> > measurements(6);
-  for (int i = 0; i < 6; ++i) {
+  vector<Vector4d, aligned_allocator<Vector4d>> measurements(6);
+  for (int i = 0; i < 6; ++i)
+  {
     Isometry3d cam_pose_inv = cam_poses[i].inverse();
-    Vector3d p = cam_pose_inv.linear()*feature + cam_pose_inv.translation();
+    Vector3d p = cam_pose_inv.linear() * feature + cam_pose_inv.translation();
     double u = p(0) / p(2) + noise_generator.gaussian(0.0, 0.01);
     double v = p(1) / p(2) + noise_generator.gaussian(0.0, 0.01);
     //double u = p(0) / p(2);
@@ -90,11 +92,12 @@ TEST(FeatureInitializeTest, sphereDistribution) {
     measurements[i] = Vector4d(u, v, u, v);
   }
 
-  for (int i = 0; i < 6; ++i) {
+  for (int i = 0; i < 6; ++i)
+  {
     cout << "pose " << i << ":" << endl;
     cout << "orientation: " << endl;
     cout << cam_poses[i].linear() << endl;
-    cout << "translation: "  << endl;
+    cout << "translation: " << endl;
     cout << cam_poses[i].translation().transpose() << endl;
     cout << "measurement: " << endl;
     cout << measurements[i].transpose() << endl;
@@ -117,8 +120,8 @@ TEST(FeatureInitializeTest, sphereDistribution) {
   EXPECT_NEAR(error.norm(), 0, 0.05);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
